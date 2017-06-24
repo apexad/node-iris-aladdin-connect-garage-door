c8b0897798087d6599dde565f80d408a6dd70c4f
apexad <apexad@icloud.com>
2017-06-24 16:46:11 -0700

    Initial commit, v0.0.1

---
+++ README.md
@@ -0,0 +1,22 @@
+# Node Iris by Lowe's tool for Aladdin Connect Garage Door
+
+This tool can be used to check the `Status` and `Open`/`Close` an Aladdin Connect Garage Door connected to Iris by Lowe's via a node script.  
+Aladdin Connect Garage Door must be setup on Iris By Lowe's([Instructions](http://www.geniecompany.com/data/products/aladdinconnect/iris-programming_aladdin-connect.pdf)).  
+To use:
+```javascript
+var iris = require('node-iris-aladdin-connect-garage-door');
+
+iris.irisGarageDoor(IRIS_USERNAME, IRIS_PASSWORD, ACTION);
+```
+Parameter           | Description
+----------------|------------
+IRIS_USERNAME   | Your Iris By Lowe's Username (usually an email address)
+IRIS_PASSWORD   | Your Iris By Lowe's Password
+ACTION          | status, open, or close
+
+## Limitations
+- Not tested with any other garage door controllers connected to Iris, but I assume it should work.
+- Not setup for multiple places/locations in Iris by Lowe's or multiple garage doors (uses the first place it finds, and the first garage door it finds).
+
+## Credits
+Uses Web Socket connections string/syntax information from [Iris Web Portal](https://github.com/thegillion/Iris-Web-Portal)
---
+++ index.js
@@ -0,0 +1,85 @@
+var rp = require('request-promise-native');
+var WebSocket = require('faye-websocket');
+var garageDoor;
+var placeId;
+var cookieData;
+var irisWebSocket;
+
+module.exports.irisGarageDoor = function (iris_user, iris_password, action) {
+  var options = {
+      method: 'POST',
+      uri: 'https://bc.irisbylowes.com/login',
+      form: {
+          user: iris_user,
+          password: iris_password,
+          public: true
+      },
+      headers: {
+          'content-type': 'application/x-www-form-urlencoded'
+      },
+      resolveWithFullResponse: true
+  };
+
+  function setPlace() {
+    irisWebSocket.send(JSON.stringify({ type: 'sess:SetActivePlace',
+      headers: { destination: 'SERV:sess:', correlationId: '78f7d29a-222e-4976-9d2b-d1f553cf8881', isRequest: true },
+      payload: { messageType: 'sess:SetActivePlace', attributes: { placeId: placeId } }
+    }));
+    listDevices();
+  }
+
+  function listDevices() {
+    irisWebSocket.send(JSON.stringify({ type: 'place:ListDevices',
+      headers: { destination: 'SERV:place:' + placeId, correlationId: '6606672e-57f8-47d1-8002-5fe59d34c1d8', isRequest: true },
+      payload: { messageType: 'place:ListDevices', attributes: {} }
+    }));
+  }
+
+  function toggleGarageDoor(setGarageState) {
+    irisWebSocket.send(JSON.stringify({
+      headers: { destination: garageDoor['base:address'], correlationId: '790525f5-171f-4533-a952-0dcafb9b5310', isRequest: true },
+      payload: { messageType: 'base:SetAttributes', attributes: { 'motdoor:doorstate': setGarageState } }
+    }));
+  }
+
+  function onMessage(messageData) {
+    if (messageData.headers.correlationId === '6606672e-57f8-47d1-8002-5fe59d34c1d8') {
+      messageData.payload.attributes.devices.forEach(function(device) {
+      	if (device['motdoor:doorstate']) {
+      	  garageDoor = device;
+      	}
+      });
+      switch (action) {
+        case 'open':
+          toggleGarageDoor('OPEN');
+          break;
+        case 'close':
+          toggleGarageDoor('CLOSED');
+          break;
+        default:
+        case 'status':
+          console.log('Garage Door State: ' + garageDoor['motdoor:doorstate']);
+      }
+      //process.exit();
+    }
+    if (messageData.payload.attributes.places) {
+      placeId = messageData.payload.attributes.places[0].placeId;
+      setPlace();
+    }
+  }
+
+  rp(options).then(function (response) {
+    cookieData = response.headers['set-cookie'][0].split(';')[0];
+    irisWebSocket = new WebSocket.Client(
+      'wss://bc.irisbylowes.com/websocket',
+      [],
+      { headers: { Cookie: cookieData} }
+    );
+    irisWebSocket.on('message', function (evt) {
+      onMessage(JSON.parse(evt.data));
+    });
+  })
+  .catch(function (err) {
+    console.log('Could not get Iris By Lowe\'s cookie');
+  });
+}
